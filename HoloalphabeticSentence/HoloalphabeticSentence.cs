b8ec116b0a7f01090ad6ebde042eb7148e360754
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-07-19 15:22:22 +0530

    First commit

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,6 +8,14 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
+            alphabets = alphabets.ToUpper();
+            for (int i = 0; i < alphabets.Length; i++)
+            {
+                if(input.ToUpper().Contains(alphabets[i]) && i==alphabets.Length-1)
+                    return true;
+                if (input.ToUpper().Contains(alphabets[i]) == false)
+                    break;
+            }
             return false;
         }
     }
